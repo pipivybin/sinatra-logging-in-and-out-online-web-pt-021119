e97c2747eb7a7b6561f1ef3dd2e4ce648029f958
Ming Duan <11203duan@gmail.com>
2019-05-30 18:28:50 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,12 +11,12 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-    
+
     @user = User.find_by(username: params[:username], password: params[:password])
     if @user
       session[:user_id] = @user.id
       redirect "/account"
-    end
+    else
       erb :error
 
   end
